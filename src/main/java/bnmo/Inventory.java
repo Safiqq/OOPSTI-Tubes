ab137ffbb94e6489a598720375dcd8cdc6ad4d10
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2023-04-19 06:13:47 +0700

    add Inventory

---
+++ src/main/java/bnmo/Inventory.java
@@ -0,0 +1,43 @@
+package bnmo;
+
+public class Inventory {
+    private Box<NonFood> boxNonFood;
+    private Box<Groceries> boxGroceries;
+    private Box<Food> boxFood;
+
+    Inventory() {
+        boxNonFood = new Box<NonFood>();
+        boxGroceries = new Box<Groceries>();
+        boxFood = new Box<Food>();
+    }
+
+    Inventory(Box<NonFood> boxNonFood, Box<Groceries> boxGroceries, Box<Food> boxFood) {
+        this.boxNonFood = boxNonFood;
+        this.boxGroceries = boxGroceries;
+        this.boxFood = boxFood;
+    }
+
+    public Box<NonFood> getBoxNonFood() {
+        return boxNonFood;
+    }
+
+    public Box<Groceries> getBoxGroceries() {
+        return boxGroceries;
+    }
+
+    public Box<Food> getBoxFood() {
+        return boxFood;
+    }
+
+    public void setBoxNonFood(Box<NonFood> boxNonFood) {
+        this.boxNonFood = boxNonFood;
+    }
+
+    public void setBoxGroceries(Box<Groceries> boxGroceries) {
+        this.boxGroceries = boxGroceries;
+    }
+
+    public void setBoxFood(Box<Food> boxFood) {
+        this.boxFood = boxFood;
+    }
+}
