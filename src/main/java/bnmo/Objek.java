18b63a8bd0d7b17010e11c4e5e6601201454231a
nixondeflin <89503892+nixondeflin@users.noreply.github.com>
2023-04-19 20:28:07 +0700

    Create Objek.java

---
+++ src/main/java/bnmo/Objek.java
@@ -0,0 +1,13 @@
+package bnmo;
+
+public class Objek {
+    private String objekName;
+
+    public String getObjekName(){
+        return objekName;
+    }
+
+    public void setObjekName(String objekName){
+        this.objekName = objekName;
+    }
+}
