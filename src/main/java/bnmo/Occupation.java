dfab81e86c47b3df845a4535d33a25280c3692d3
raniakiranaa <16521042@std.stei.itb.ac.id>
2023-04-20 01:47:30 +0700

    add occupation

---
+++ src/main/java/bnmo/Occupation.java
@@ -0,0 +1,53 @@
+package bnmo;
+
+import java.util.*;
+
+public class Occupation {
+    private String jobName;
+    private int dailySalary;
+    private Map<String, Integer> listJob = new HashMap<>();
+    private List<String> keys;
+
+    public Occupation(){
+        fillListJob();
+        keys = new ArrayList<>(listJob.keySet());
+        Random random = new Random();
+        jobName = keys.get(random.nextInt(keys.size()));
+        dailySalary = listJob.get(jobName); 
+    }
+
+    private void fillListJob(){
+        listJob.put("Badut Sulap", 15);
+        listJob.put("Koki", 30);
+        listJob.put("Polisi", 35);
+        listJob.put("Programmer", 45);
+        listJob.put("Dokter", 50);   
+        listJob.put("Ojek Payung", 1);
+        listJob.put("Pawang Hujan", 5);
+        listJob.put("Pelukis", 20);
+        listJob.put("Tukang Parkir", 2);
+        listJob.put("Pengusaha", 60);
+    }
+
+    public void changeJob(){
+        Random random = new Random();
+        boolean done = false;
+        String oldJobName = jobName;
+        while (!done){
+            jobName = keys.get(random.nextInt(keys.size()));
+            if(!jobName.equals(oldJobName)){
+                done = true;
+            }
+        }
+
+        dailySalary = listJob.get(jobName); 
+    }
+    
+    public String getJobName(){
+        return jobName;
+    }
+
+    public int getDailySalary(){
+        return dailySalary;
+    }
+}
---
+++ src/test/java/bnmo/OccupationTest.java
@@ -0,0 +1,70 @@
+package bnmo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+
+public class OccupationTest {
+    private static Occupation occupation;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        Occupation occupation = new Occupation();
+    }
+
+    @Test
+    public void testOccupationConstructor(){
+        String job = occupation.getJobName();
+        if (job.equals("Badut Sulap")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 15);
+        } else if (job.equals("Koki")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 30);
+        } else if (job.equals("Polisi")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 35);
+        } else if (job.equals("Programmer")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 45);
+        } else if (job.equals("Dokter")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 50);
+        } else if (job.equals("Ojek Payung")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 1);
+        } else if (job.equals("Pawang Hujan")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 5);
+        } else if (job.equals("Pelukis")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 20);
+        } else if (job.equals("Tukang Parkir")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 2);
+        } else if (job.equals("Pengusaha")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 60);
+        }
+    }
+
+    @Test
+    public void testOccupationSet(){
+        String oldJob = occupation.getJobName();
+        occupation.changeJob();
+        assertFalse("job stays the same", oldJob.equals(occupation.getJobName()));
+        String job = occupation.getJobName();
+        if (job.equals("Badut Sulap")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 15);
+        } else if (job.equals("Koki")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 30);
+        } else if (job.equals("Polisi")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 35);
+        } else if (job.equals("Programmer")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 45);
+        } else if (job.equals("Dokter")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 50);
+        } else if (job.equals("Ojek Payung")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 1);
+        } else if (job.equals("Pawang Hujan")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 5);
+        } else if (job.equals("Pelukis")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 20);
+        } else if (job.equals("Tukang Parkir")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 2);
+        } else if (job.equals("Pengusaha")){
+            assertEquals("salary not equal", occupation.getDailySalary(), 60);
+        }
+    }
+}
