777f39f8e409172b1c31d2baff03db790100a9b0
raniakiranaa <16521042@std.stei.itb.ac.id>
2023-04-20 00:58:40 +0700

    add location, locationtest nunggu house, room

---
+++ src/main/java/bnmo/Location.java
@@ -0,0 +1,37 @@
+package bnmo;
+
+public class Location {
+    private House house;
+    private Room room;
+    private Point point;
+
+    public Location(House house, Room room, Point point){
+        this.house = house;
+        this.room = room;
+        this.point = point;
+    }
+
+    public House getHouse(){
+        return house;
+    }
+
+    public Room getRoom(){
+        return room;
+    }
+
+    public Point getPoint(){
+        return point;
+    }
+
+    public void setHouse(House house){
+        this.house = house;
+    }
+
+    public void setRoom(Room room){
+        this.room = room;
+    }
+
+    public void setPoint(Point point){
+        this.point = point;
+    }
+}
