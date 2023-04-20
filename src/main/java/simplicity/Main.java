abd7693bbfa072850a62ef728b195f0e652927e6
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2023-04-20 15:31:49 +0700

    add menu

--- src/main/java/simplicity/Main.java
+++ src/main/java/simplicity/Main.java
@@ -2,7 +2,7 @@ package simplicity;
 
 import javax.swing.*;
 
-public class App {
+public class Main {
 
     public static class cobaJavaSwing {
         public static void start() {
@@ -16,12 +16,29 @@ public class App {
         }
     }
 
-    public String getGreeting() {
-        return "Hello world.";
+    public void showMenu() {
+        StringBuilder menu = new StringBuilder();
+        menu.append("Menu Game:\n");
+        menu.append("1. Start Game\n");
+        menu.append("2. Help\n");
+        menu.append("3. Exit\n");
+        menu.append("4. View Sim Info\n");
+        menu.append("5. View Current Location\n");
+        menu.append("6. View Inventory\n");
+        menu.append("7. Upgrade House\n");
+        menu.append("8. Move Room\n");
+        menu.append("9. Edit Room\n");
+        menu.append("10. Add Sim\n");
+        menu.append("11. Change Sim\n");
+        menu.append("12. List Object\n");
+        menu.append("13. Go to Object\n");
+        menu.append("14. Action\n");
+        System.out.println(menu);
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-        cobaJavaSwing.start();
+        // cobaJavaSwing.start();
+        Main main = new Main();
+        main.showMenu();
     }
 }
