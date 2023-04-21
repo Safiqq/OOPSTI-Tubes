3271187b76a4d4b45eacf376a929156f462e5c57
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2023-04-21 14:37:31 +0700

    apply Singleton design pattern

--- src/main/java/simplicity/World.java
+++ src/main/java/simplicity/World.java
@@ -1,60 +1,63 @@
 package simplicity;
 
 public class World {
-    //attribute
+    // attribute
+    private static World world = new World();
     private final int worldLength = 64;
     private final int worldWidth = 64;
     private House[][] matrixHouse;
 
-    //constructor
-    public World(){
+    // constructor
+    private World() {
         matrixHouse = new House[worldLength][worldWidth];
-        for (int i = 0 ; i < worldWidth ; i++){    
-            for(int j = 0 ; j < worldLength ; j++){
+        for (int i = 0; i < worldWidth; i++) {
+            for (int j = 0; j < worldLength; j++) {
                 matrixHouse[i][j] = null;
             }
         }
     }
 
-    //methods getter
-    public int getWorldLength(){
+    public static World getWorld() {
+        return world;
+    }
+
+    // methods getter
+    public int getWorldLength() {
         return worldLength;
     }
 
-    public int getWorldWidth(){
+    public int getWorldWidth() {
         return worldWidth;
     }
 
-    public House[][] getMatrixHouse(){
+    public House[][] getMatrixHouse() {
         return matrixHouse;
     }
 
-    //method setter
-    public void setMatrixHouse(House[][] matrixHouse){
+    // method setter
+    public void setMatrixHouse(House[][] matrixHouse) {
         this.matrixHouse = matrixHouse;
     }
 
-    //other methods
+    // other methods
 
-    public void addHouse(House house, Point point) throws Exception{
-    //add house ke world
-        try{
-            if (matrixHouse[point.getX()][point.getY()] != null){
+    public void addHouse(House house, Point point) throws Exception {
+        // add house ke world
+        try {
+            if (matrixHouse[point.getX()][point.getY()] != null) {
                 matrixHouse[point.getX()][point.getY()] = house;
-            }
-            else{
+            } else {
                 throw new Exception("Sudah ada rumah Sim lain");
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void printMatrixHouse(){
-    //menampilkan world
-        for (int i = 0 ; i < worldWidth ; i++){    
-            for(int j = 0 ; j < worldLength ; j++){
+    public void printMatrixHouse() {
+        // menampilkan world
+        for (int i = 0; i < worldWidth; i++) {
+            for (int j = 0; j < worldLength; j++) {
                 System.out.print(matrixHouse[i][j]);
                 System.out.print(" ");
             }
