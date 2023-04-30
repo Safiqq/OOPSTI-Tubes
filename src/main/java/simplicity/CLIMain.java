d14d5cfb606db4758d5c9aef255ea6a5e55567d9
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2023-05-01 03:44:16 +0700

    pisah main jadi gui dan cli

---
+++ src/main/java/simplicity/CLIMain.java
@@ -0,0 +1,427 @@
+package simplicity;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CLIMain extends Main {
+    private static Scanner scanner;
+
+    private static boolean isStarted;
+
+    public CLIMain() {
+        super();
+        scanner = new Scanner(System.in);
+        time = new Time();
+        dayAddSim = 0;
+        isStarted = false;
+    }
+
+    public void start() {
+        // Inisialisasi main
+        CLIMain main = new CLIMain();
+        Print.showMenuBegin();
+        System.out.println("Ketik 'START GAME' untuk memulai game atau 'HELP' untuk melihat menu game");
+
+        while (!isStarted) {
+            System.out.print("Masukkan perintah: ");
+            String menu = scanner.nextLine();
+            if (equals(menu, "START GAME")) {
+                isStarted = true;
+            } else if (equals(menu, "EXIT")) {
+                System.out.println("Anda keluar dari game Simplicity");
+                System.out.println("Bye...");
+                System.exit(0);
+            } else if (equals(menu, "HELP")) {
+                Print.showMenuBegin();
+            } else {
+                System.out.println("Perintah tidak valid");
+            }
+        }
+
+        System.out.println(time.getTime());
+        // pilih Sim
+        if (Sim.getListSim().size() == 0) {
+            System.out.println("Tidak ada Sim yang tersedia, Silakan daftarkan Sim anda terlebih dahulu");
+            System.out.print("Masukkan nama Sim anda: ");
+            String simName = scanner.nextLine();
+            Sim newSim = new Sim(simName);
+            House newHouse = new House(simName);
+            world.addHouse(newHouse);
+            currentSim = newSim;
+            System.out.println("Selamat bermain!");
+
+        } else {
+            System.out.print("Apakah anda ingin membuat Sim baru? (ya/tidak) ");
+            String ans = scanner.nextLine();
+
+            if (equals(ans, "YA")) {
+                // cek masi ada spot kosong di world ato ngga
+                if (world.isHouseBuildAble()) {
+                    boolean done = false;
+                    Sim newSim;
+                    while (!done) {
+                        newSim = menuAddSim();
+                        if (newSim != null) {
+                            currentSim = newSim;
+                            System.out.println("Selamat bermain!");
+                            done = true;
+                        } else {
+                            System.out.println("Silakan mencoba mendaftarkan Sim baru di lokasi yang berbeda");
+                        }
+                    }
+                } else {
+                    System.out.println("World penuh, tidak memungkinkan membuat Sim baru");
+                    System.out.println("Silakan memilih Sim yang sudah ada");
+                    currentSim = chooseSim();
+                    System.out.println("Selamat bermain!");
+                }
+
+            } else { // jawaban selain ya dan tidak dianggap tidak
+                currentSim = chooseSim();
+                System.out.println("Selamat bermain!");
+            }
+        }
+
+        System.out.println("1Waktu yang tersisa di " + time.getTime());
+        // setCurrentTime(time);
+
+        System.out.println("Ketik 'HELP' untuk melihat menu game yang tersedia ");
+        while (isStarted) {
+            // cek apakah currentSim masih ada di list Sim (tidak dihapus karena mati)
+            boolean checked = false;
+            while (!checked) {
+                if (Sim.getListSim().contains(main.currentSim)) {
+                    checked = true;
+                } else {
+                    System.out.println("Sim yang anda mainkan mati karena depresi, silahkan lakukan penggantian Sim");
+                    if (Sim.getListSim().size() == 0) {
+                        System.out.println(
+                                "Tidak bisa dilakukan pergantian Sim. Tidak terdapat Sim lain yang terdaftar.");
+                        System.out.println("Silahkan daftar Sim baru");
+                        System.out.print("Masukkan nama Sim anda: ");
+                        String simName = scanner.nextLine();
+                        Sim newSim = new Sim(simName);
+                        world.addHouse(newSim.getSimLoc().getHouse());
+                        main.currentSim = newSim;
+                        System.out.println("Selamat bermain!");
+                    } else {
+                        main.currentSim = main.chooseSim();
+                        System.out.println("Selamat bermain!");
+                    }
+                }
+            }
+
+            System.out.println("2Waktu yang tersisa di " + time.getTime());
+            System.out.print("Masukkan perintah: ");
+            String menu = scanner.nextLine();
+            if (equals(menu, "HELP")) {
+                Print.showMenu();
+
+            } else if (equals(menu, "EXIT")) {
+                System.out.println("Anda keluar dari game Simplicity");
+                System.out.println("3Waktu yang tersisa di " + time.getTime());
+                System.out.println("Bye...");
+                System.exit(0);
+
+            } else if (equals(menu, "VIEW SIM INFO")) {
+                currentSim.viewSimInfo();
+
+            } else if (equals(menu, "VIEW CURRENT LOCATION")) {
+                currentSim.viewSimLoc();
+
+            } else if (equals(menu, "VIEW INVENTORY")) {
+                currentSim.viewSimInventory();
+
+            } else if (equals(menu, "UPGRADE HOUSE")) {
+                // currentSim.upgradeHouse(house);
+                // selama 18 menit tapi bisa ditinggal
+                time.sleep(1080);
+
+            } else if (equals(menu, "MOVE ROOM")) {
+                currentSim.moveRoom();
+
+            } else if (equals(menu, "EDIT ROOM")) {
+                boolean done = false;
+                while (!done) {
+                    System.out.print("Apakah anda ingin membeli barang baru atau memindahkan barang? (beli/pindah)");
+                    String ans = scanner.nextLine();
+                    if (equals(ans, "BELI")) {
+
+                        done = true;
+                    } else if (equals(ans, "PINDAH")) {
+
+                        done = true;
+                    } else {
+                        System.out.println("Perintah tidak valid");
+                    }
+                }
+
+            } else if (equals(menu, "ADD SIM")) {
+                // hanya dapat dilakukan 1 hari sekali
+                if (time.getDay() > dayAddSim) {
+                    // cek masi ada spot kosong di world ato ngga
+                    if (world.isHouseBuildAble()) {
+                        boolean done = false;
+                        Sim newSim;
+                        while (!done) {
+                            newSim = menuAddSim();
+                            if (newSim != null) {
+                                dayAddSim = time.getDay(); // hanya dapat dilakukan 1 hari sekali
+                                done = true;
+                            } else {
+                                System.out.println("Silakan mencoba mendaftarkan Sim baru di lokasi yang berbeda");
+                            }
+                        }
+                    } else {
+                        System.out.println("World penuh, tidak memungkinkan membuat Sim baru");
+                    }
+                } else {
+                    System.out.println("Menu 'ADD SIM' hanya dapat dijalankan 1 hari sekali");
+                }
+
+            } else if (equals(menu, "CHANGE SIM")) {
+                // di listSim hanya ada 1 sim
+                if (Sim.getListSim().size() == 1) {
+                    System.out.println("Tidak bisa dilakukan pergantian Sim. Hanya terdapat 1 Sim yang terdaftar.");
+                    System.out.println("Ketik 'ADD SIM' untuk menambah Sim baru");
+                } else {
+                    Sim oldSim = currentSim;
+                    boolean done = false;
+                    Sim newSim = null;
+                    while (!done) {
+                        newSim = chooseSim();
+                        if (equals(newSim.getFullName(), oldSim.getFullName())) {
+                            System.out.println("Nama Sim sama dengan yang sedang anda mainkan");
+                            System.out.println("Masukkan nama Sim yang berbeda");
+                        } else {
+                            done = true;
+                        }
+                    }
+                    currentSim = newSim;
+                    System.out.println("Sim berhasil diganti. Selamat bermain!");
+                }
+
+            } else if (equals(menu, "LIST OBJECT")) {
+                // menampilkan daftar objek dalam sebuah ruangan
+                // asumsi: sim harus berada di dalam ruangan untuk melihat daftar objek dalam
+                // ruangan tsb
+                ArrayList<NonFood> listObjek = currentSim.getSimLoc().getRoom().getListObjek();
+                if (listObjek.size() > 0) {
+                    int i = 1;
+                    for (NonFood nonFood : listObjek) {
+                        System.out.println(i + ". " + nonFood.getObjekName());
+                        i++;
+                    }
+                } else {
+                    System.out.println("Tidak ada daftar objek dalam ruangan "
+                            + currentSim.getSimLoc().getRoom().getRoomName() + ".");
+                }
+
+            } else if (equals(menu, "GO TO OBJECT")) {
+
+            } else if (equals(menu, "HESOYAM")) {
+                currentSim.setMoney(9999999);
+                System.out.println("Selamat! Kamu mendapatkan uang jajan dari Hotman Paris");
+
+            } else if (equals(menu, "ACTION")) {
+                Print.showAction();
+                System.out.print("Masukkan aksi yang ingin dijalankan: ");
+                String act = scanner.nextLine();
+
+                if (equals(act, "WORK")) {
+                    // pekerjaan baru hanya dapat dikerjakan 1 hari setelah hari penggantian
+                    // pekerjaan
+                    if (time.getDay() > currentSim.getDayChangeJob()) {
+                        System.out.print("Masukkan durasi kerja (dalam detik): ");
+                        int simWorkTime = scanner.nextInt();
+                        currentSim.work(simWorkTime);
+                    } else {
+                        System.out.println(
+                                "Pekerjaan baru hanya dapat dikerjakan 1 hari setelah hari penggantian pekerjaan");
+                    }
+
+                } else if (equals(act, "CHANGE JOB")) { // action ato taruh di work?
+                    // harus setidaknya 12 menit bekerja
+                    if (currentSim.getWorkTime() >= 720) {
+                        currentSim.newJob();
+                        currentSim.setDayChangeJob(time.getDay());
+                    } else {
+                        System.out
+                                .println("Sim hanya dapat mengganti pekerjaan jika sudah bekerja setidaknya 12 menit");
+                    }
+
+                } else if (equals(act, "EXERCISE")) {
+                    System.out.print("Masukkan durasi olahraga (dalam detik): ");
+                    int simExerciseTime = scanner.nextInt();
+                    currentSim.exercise(simExerciseTime);
+
+                } else if (equals(act, "SLEEP")) {
+                    // sim sebagai manusia harus memiliki waktu tidur min 3 mnt setiap harinya
+                    // efek tidak tidur -> -5 kesehatan dan -5 mood setelah 10 mnt tanpa tidur
+                    // apakah harus 3 menit langsung atau boleh dicicil?
+                    // penambahan efek tidur apakah akumulasi dalam hari tersebut atau langsung
+                    // dibagi tiap tidur
+
+                    System.out.print("Masukkan durasi tidur (dalam detik): ");
+                    int simSleepTime = scanner.nextInt();
+                    currentSim.sleep(simSleepTime);
+                    time.sleepMain(simSleepTime);
+
+                } else if (equals(act, "EAT")) {
+                    currentSim.eat();
+
+                } else if (equals(act, "COOK")) {
+                    Print.showCookingMenu();
+                    currentSim.cook();
+
+                } else if (equals(act, "VISIT")) {
+                    // mau masukin visit rumah orang pake nama owner?
+                    boolean done = false;
+                    Point houseLoc = null;
+                    while (!done) {
+                        System.out.print("Masukkan nama pemilik rumah yang ingin dikunjungi: ");
+                        String ownerHouse = scanner.nextLine();
+                        houseLoc = world.searchHouse(ownerHouse);
+                        if (houseLoc == null) {
+                            System.out.println("Tidak ada rumah yang dimiliki oleh " + ownerHouse);
+                            Print.printListSim();
+                        } else {
+                            done = true;
+                        }
+                    }
+
+                    System.out.print("Masukkan durasi berkunjung (dalam detik): ");
+                    int simVisitTime = scanner.nextInt();
+                    currentSim.visit(houseLoc, simVisitTime);
+
+                } else if (equals(act, "PEE")) {
+                    // sim minimal buang air 1 kali tiap habis makan
+                    // efek tidak buang air: -5 kesehatan dan -5 mood 4 menit setelah makan tanpa
+                    // buang air -> gimana
+
+                    currentSim.pee();
+                    time.sleepMain(10);
+
+                } else if (equals(act, "UPGRADE HOUSE")) {
+                    // currentSim.upgradeHouse(house);
+                    // selama 18 menit tapi bisa ditinggal
+                    time.sleep(1080);
+
+                } else if (equals(act, "BUY ITEM")) {
+                    Print.showBuyObjectMenu();
+                    currentSim.buyItem();
+
+                } else if (equals(act, "MOVE ROOM")) {
+                    currentSim.moveRoom();
+
+                } else if (equals(act, "VIEW INVENTORY")) {
+                    System.out.println(time.getTime());
+                    currentSim.viewSimInventory();
+
+                } else if (equals(act, "INSTALL ITEM")) {
+
+                } else if (equals(act, "CHECK TIME")) {
+                    // pake harus ngehampirin objek jam dulu
+                    System.out.println("4Waktu yang tersisa di- " + time.getTime());
+                    // sisa waktu yang masih ada untuk seluruh tindakan yang bisa ditinggal
+
+                } else {
+                    System.out.println("Aksi tidak valid");
+                }
+            } else {
+                System.out.println("Perintah tidak valid");
+            }
+        }
+        scanner.close();
+    }
+
+    public void clearScreen() {
+        try {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows"))
+                Runtime.getRuntime().exec("cls");
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Sim chooseSim() {
+        Print.printListSim();
+        boolean done = false;
+        String simName;
+        while (!done) {
+            System.out.print("Masukkan nama Sim yang ingin dimainkan: ");
+            simName = scanner.nextLine();
+            if (isNotRegistered(simName)) {
+                System.out.println("Sim tidak ditemukan di list Sim");
+                Print.printListSim();
+            } else {
+                // ambil Sim di list
+                for (Sim sim : Sim.getListSim()) {
+                    if (equals(simName, sim.getFullName())) {
+                        return sim;
+                    }
+                }
+                done = true;
+            }
+        }
+        return null;
+    }
+
+    public boolean isNotRegistered(String simName) {
+        for (Sim sim : Sim.getListSim()) {
+            if (equals(simName, sim.getFullName())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Sim menuAddSim() {
+        // Validasi nama Sim
+        boolean found = true;
+        String simName = null;
+        while (found) {
+            System.out.print("Masukkan nama Sim yang ingin anda tambahkan: ");
+            simName = scanner.nextLine();
+            if (isNotRegistered(simName)) {
+                found = false;
+            } else {
+                System.out.println("Nama Sim telah terdaftar. Silakan menggunakan nama lain");
+                Print.printListSim();
+            }
+        }
+
+        // validasi lokasi rumah Sim
+        world.printMatrixHouse();
+        System.out.println("Masukkan titik untuk mendirikan rumah: ");
+        boolean done = false;
+        int x = 0, y = 0;
+        while (!done) {
+            System.out.print("X: ");
+            x = scanner.nextInt();
+            System.out.print("Y: ");
+            y = scanner.nextInt();
+            if ((x < 0 || x > 64) || (y < 0 || y > 64)) {
+                System.out.println("Titik tidak valid. World berukuran 64x64");
+            } else {
+                done = true;
+            }
+        }
+
+        Point houseLoc = new Point(x, y);
+        // cek fungsi isWorldAvail
+        if (world.isWorldAvail(houseLoc)) {
+            Sim newSim = new Sim(simName);
+            world.addHouse(newSim.getFullName(), houseLoc);
+            System.out.println("Sim berhasil didaftarkan");
+            return newSim;
+        } else {
+            System.out.println("Tidak memungkinkan untuk membuat rumah baru di lokasi tersebut");
+            System.out.println("Pendaftaran Sim baru gagal");
+            return null;
+        }
+    }
+}
--- src/main/java/simplicity/GUIMain.java
+++ src/main/java/simplicity/GUIMain.java
@@ -1,17 +1,218 @@
 package simplicity;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
-public class GUIMain {
-    public static class cobaJavaSwing {
-        public static void start() {
-            JFrame f = new JFrame("Sim-Plicity");
-            JButton b = new JButton("Start");
-            b.setBounds(130, 100, 100, 40);
-            f.add(b);
-            f.setSize(400, 500);
-            f.setLayout(null);// using no layout managers
-            f.setVisible(true);// making the frame visible
+public class GUIMain extends Main {
+    private final static String basePath = "src/main/resources";
+    private final static String imagePath = basePath + "/images";
+    private final static String fontPath = basePath + "/fonts";
+
+    public GUIMain() {
+        super();
+    }
+
+    public void start() {
+        GUIMain main = new GUIMain();
+        main.importFont();
+        main.showMainFrame();
+    }
+
+    public void importFont() {
+        final GraphicsEnvironment GE = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        final List<String> AVAILABLE_FONT_FAMILY_NAMES = Arrays.asList(GE.getAvailableFontFamilyNames());
+        try {
+            final File fontFile = new File(fontPath, "SimsLLHP.ttf");
+            if (fontFile.exists()) {
+                Font FONT = Font.createFont(Font.TRUETYPE_FONT, fontFile);
+                if (!AVAILABLE_FONT_FAMILY_NAMES.contains(FONT.getFontName())) {
+                    GE.registerFont(FONT);
+                }
+            }
+        } catch (FontFormatException | IOException exception) {
+            JOptionPane.showMessageDialog(null, exception.getMessage());
         }
+    }
+
+    public void showMainFrame() {
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(1280, 720);
+        frame.setResizable(false);
+
+        BufferedImage img;
+        try {
+            img = ImageIO.read(new File(imagePath, "startScreen.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        ImageIcon icon = resizeImage(new ImageIcon(img), frame.getWidth(), frame.getHeight());
+
+        JLabel label0 = new JLabel();
+        label0.setIcon(icon);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(createButton("Start Game", e -> {
+            String simName = JOptionPane.showInputDialog(frame, "Masukkan nama SIM:");
+            currentSim = new Sim(simName);
+            frame.setVisible(false);
+            showWorldFrame();
+        }));
+        panel.add(createButton("About", e -> {
+            frame.setVisible(false);
+            showAboutFrame();
+        }));
+
+        frame.add(label0);
+        frame.add(panel);
+
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public JPanel createSimInfo() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel label0 = createLabel("Nama Sim: " + currentSim.getFullName());
+        JLabel label1 = createLabel("Pekerjaan Sim: " + currentSim.getOccupation().getJobName());
+        JLabel label2 = createLabel("Kesehatan Sim: " + currentSim.getMotive().getHealth());
+        JLabel label3 = createLabel("Kekenyangan Sim: " + currentSim.getMotive().getHunger());
+        JLabel label4 = createLabel("Mood Sim: " + currentSim.getMotive().getMood());
+        JLabel label5 = createLabel("Uang Sim: " + currentSim.getMoney());
+
+        panel.add(label0);
+        panel.add(label1);
+        panel.add(label2);
+        panel.add(label3);
+        panel.add(label4);
+        panel.add(label5);
+        return panel;
+    }
+
+    public JLabel createLabel(String text) {
+        return createLabel(text, Component.LEFT_ALIGNMENT, 20);
+    }
+
+    public JLabel createLabel(String text, float alignment) {
+        return createLabel(text, alignment, 20);
+    }
+
+    public JLabel createLabel(String text, int fontSize) {
+        return createLabel(text, Component.LEFT_ALIGNMENT, fontSize);
+    }
+
+    public JLabel createLabel(String text, float alignment, int fontSize) {
+        JLabel label = new JLabel(text);
+        label.setAlignmentX(alignment);
+        label.setFont(new Font("SimLLHP", Font.PLAIN, fontSize));
+        return label;
+    }
+
+    public JButton createButton(String text, ActionListener callback) {
+        JButton button = new JButton(text);
+        button.addActionListener(callback);
+        button.setFont(new Font("SimLLHP", Font.PLAIN, 20));
+        return button;
+    }
+
+    public void showAboutFrame() {
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setResizable(false);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        panel.add(createLabel("IF2212 Pemrograman Berorientasi Objek STI", Component.CENTER_ALIGNMENT));
+        panel.add(createLabel(" "));
+        panel.add(createLabel("Kelompok 8", Component.CENTER_ALIGNMENT));
+        panel.add(createLabel("1. Michael Ryan Martin (18219046)", Component.CENTER_ALIGNMENT));
+        panel.add(createLabel("2. Syafiq Ziyadul Arifin (18221048)", Component.CENTER_ALIGNMENT));
+        panel.add(createLabel("3. Nixon Deflin Kurniawan (18221150)", Component.CENTER_ALIGNMENT));
+        panel.add(createLabel("4. Fredrick Runie Taslim (18221156)", Component.CENTER_ALIGNMENT));
+        panel.add(createLabel("5. Rania Sasi Kirana (18221168)", Component.CENTER_ALIGNMENT));
+
+        frame.add(panel);
+        frame.add(createButton("Back", e -> {
+            frame.setVisible(false);
+            showMainFrame();
+        }));
+
+        frame.pack();
+        frame.setSize(720, 250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public void showRoomFrame() {
+
+    }
+
+    public void showWorldFrame() {
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setResizable(false);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(65, 65));
+
+        for (int i = 0; i < (65 * 65); i++) {
+            JLabel label;
+            Point houseLoc = currentSim.getSimLoc().getHouse().getHouseLoc();
+            System.out.println("X: " + houseLoc.getX() + ", Y: " + houseLoc.getY());
+            if (i == (houseLoc.getY() * 65 + houseLoc.getX())) label = createLabel("  X", 6);
+            else label = createLabel("       ", 6);
+            label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            final int _i = i;
+            label.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    frame.setVisible(false);
+                    System.out.println("X: " + _i % 65 + ", Y: " + _i / 65);
+                }
+            });
+            panel.add(label);
+        }
+
+        frame.add(panel);
+        frame.add(createSimInfo());
+        frame.add(createButton("Back", e -> {
+            frame.setVisible(false);
+            showMainFrame();
+        }));
+
+        frame.pack();
+        frame.setSize(1280, 700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public ImageIcon resizeImage(ImageIcon currentIcon, int width, int height) {
+        int nw = currentIcon.getIconWidth();
+        int nh = currentIcon.getIconHeight();
+
+        if (currentIcon.getIconWidth() > width) {
+            nw = width;
+            nh = (nw * currentIcon.getIconHeight()) / currentIcon.getIconWidth();
+        }
+        if (nh > height) {
+            nh = height;
+            nw = (currentIcon.getIconWidth() * nh) / currentIcon.getIconHeight();
+        }
+
+        return new ImageIcon(currentIcon.getImage().getScaledInstance(nw, nh, Image.SCALE_DEFAULT));
     }
 }
