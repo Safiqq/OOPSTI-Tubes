85d2af9a88b5da6642ba41c695818a92447c5f93
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2023-05-01 03:43:48 +0700

    add new actions

--- src/main/java/simplicity/Action.java
+++ src/main/java/simplicity/Action.java
@@ -37,122 +37,98 @@ public class Action {
     public static void fillListAction() {
         /* Aksi Kerja */
         Effect[] arrayEffect1 = {new Effect("kekenyangan", -10, 30), new Effect("mood", -10, 30)};
-        listAction.add(new Action("Work",
-                "Sim harus bekerja. Selama bekerja, sim kekenyangan dan mood Sim akan berkurang. Kerja juga akan " +
-                        "menghasilkan uang dengan jumlah yang bergantung pada pekerjaan dari Sim.",
-                Arrays.asList(arrayEffect1)));
+        listAction.add(new Action("Work", "Sim harus bekerja.", Arrays.asList(arrayEffect1)));
         Arrays.fill(arrayEffect1, null);
 
         /* Aksi Olahraga */
         Effect[] arrayEffect2 = {new Effect("kesehatan", 5, 20), new Effect("kekenyangan", -5, 20),
                 new Effect("mood", 10, 20)};
-        listAction.add(new Action("Exercise",
-                "Sim dapat berolahraga untuk menaikkan kesehatan dan mood. Tetapi, olahraga akan menurunkan " +
-                        "kekenyangan.",
-                Arrays.asList(arrayEffect2)));
+        listAction.add(new Action("Exercise", "Sim dapat berolahraga", Arrays.asList(arrayEffect2)));
         Arrays.fill(arrayEffect2, null);
 
         /* Aksi Tidur */
         Effect[] arrayEffect31 = {new Effect("mood", 30, 240), new Effect("kesehatan", 20, 240)};
-        listAction.add(new Action("Sleep",
-                "SIm sebagai manusia harus memiliki waktu tidur minimum 3 menit setiap harinya atau kesehatan dari " +
-                        "sim akan berkurang.",
-                Arrays.asList(arrayEffect31)));
+        listAction.add(new Action("Sleep", "Sim sebagai manusia harus memiliki waktu tidur minimum 3 menit setiap harinya.", Arrays.asList(arrayEffect31)));
         Arrays.fill(arrayEffect31, null);
 
         /* Tidak Tidur */
         Effect[] arrayEffect32 = {new Effect("kesehatan", -5, 0), new Effect("mood", -5, 0)};
-        listAction.add(new Action("Not Sleep",
-                "SIm sebagai manusia harus memiliki waktu tidur minimum 3 menit setiap harinya atau kesehatan dari " +
-                        "sim akan berkurang.",
-                Arrays.asList(arrayEffect32)));
+        listAction.add(new Action("Not Sleep", "Sim sebagai manusia harus memiliki waktu tidur minimum 3 menit setiap harinya.", Arrays.asList(arrayEffect32)));
         Arrays.fill(arrayEffect32, null);
 
         /* Aksi Makan */
         Effect[] arrayEffect4 = {new Effect("kekenyangan", 0, 30)};
-        listAction.add(new Action("Eat",
-                "Makan berarti Sim mengambil makanan yang ada di Inventory untuk kemudian dikonsumsi. Konsumsi " +
-                        "makanan akan mengurangi jumlah makanan terkait pada inventory sejumlah 1 buah dan " +
-                        "meningkatkan tingkat kekenyangan Sim sejumlah satuan kekenyangan makanan terkait.",
-                Arrays.asList(arrayEffect4)));
+        listAction.add(new Action("Eat", "Makan berarti Sim mengambil makanan yang ada di Inventory untuk kemudian dikonsumsi.", Arrays.asList(arrayEffect4)));
         Arrays.fill(arrayEffect4, null);
 
         /* Aksi Memasak */
         Effect[] arrayEffect5 = {new Effect("mood", 10, 0)};
-        listAction.add(new Action("Cook",
-                "Sim bisa membuat makanan dengan melakukan tindakan masak. Masak akan memilih dari suatu menu, dengan" +
-                        " setiap makanan dari menu memiliki waktu pembuatan makanan serta memenuhi rasa kekenyangan " +
-                        "sejumlah satuan tertentu. Makanan yang berhasil dibuat akan dimasukkan ke dalam sebuah " +
-                        "inventory. Waktu yang diperlukan untuk memasak adalah (1.5*tingkat kekenyangan) masakan.",
-                Arrays.asList(arrayEffect5)));
+        listAction.add(new Action("Cook", "Sim bisa membuat makanan dengan melakukan tindakan masak. Makanan yang berhasil dibuat akan dimasukkan ke dalam sebuah inventory.", Arrays.asList(arrayEffect5)));
         Arrays.fill(arrayEffect5, null);
 
         /* Aksi Berkunjung */
         Effect[] arrayEffect6 = {new Effect("mood", 10, 30), new Effect("kekenyangan", -10, 30)};
-        listAction.add(new Action("Visit",
-                "Sim dapat berkunjung ke rumah lain yang berada pada world. Waktu yang dihabiskan untuk kunjungan " +
-                        "dapat berefek ke mood dari Sim. Diperlukan waktu untuk berkunjung dari titik SIM ke titik " +
-                        "rumah SIM lainnya. Perhitungan waktu tersebut dapat dilakukan dengan rumus √(x2-x1)2+(y2-y1)" +
-                        "2.",
-                Arrays.asList(arrayEffect6)));
+        listAction.add(new Action("Visit", "Sim dapat berkunjung ke rumah lain yang berada pada world.", Arrays.asList(arrayEffect6)));
         Arrays.fill(arrayEffect6, null);
 
         /* Aksi Buang Air */
         Effect[] arrayEffect71 = {new Effect("kekenyangan", -20, 10), new Effect("mood", 10, 10)};
-        listAction.add(new Action("Pee",
-                "Sim sebagai manusia virtual memiliki organ pencernaan layaknya manusia biasa. Akibatnya, setiap hari" +
-                        " ia harus buang air setidaknya 1 kali setiap habis makan. Apabila tidak dilakukan, maka mood" +
-                        " dan kesehatan Sim akan berkurang.",
-                Arrays.asList(arrayEffect71)));
+        listAction.add(new Action("Pee", "Sim sebagai manusia virtual memiliki organ pencernaan layaknya manusia biasa.", Arrays.asList(arrayEffect71)));
         Arrays.fill(arrayEffect71, null);
 
         /* Tidak Buang Air */
         Effect[] arrayEffect72 = {new Effect("kesehatan", -5, 240), new Effect("mood", -5, 240)};
-        listAction.add(new Action("Not Pee",
-                "Sim sebagai manusia virtual memiliki organ pencernaan layaknya manusia biasa. Akibatnya, setiap hari" +
-                        " ia harus buang air setidaknya 1 kali setiap habis makan. Apabila tidak dilakukan, maka mood" +
-                        " dan kesehatan Sim akan berkurang.",
-                Arrays.asList(arrayEffect72)));
+        listAction.add(new Action("Not Pee", "Sim sebagai manusia virtual memiliki organ pencernaan layaknya manusia biasa.", Arrays.asList(arrayEffect72)));
         Arrays.fill(arrayEffect72, null);
 
         /* Aksi Upgrade Rumah */
         Effect[] arrayEffect8 = {new Effect("", 0, 1080)};
-        listAction.add(new Action("Upgrade House",
-                "Sim bisa menghabiskan uang virtual untuk meningkatkan rumahnya dengan menambah ruangan. Setiap " +
-                        "penambahan ruangan membutuhkan waktu sejumlah 18 menit.",
-                Arrays.asList(arrayEffect8)));
+        listAction.add(new Action("Upgrade House", "Sim bisa menghabiskan uang virtual untuk meningkatkan rumahnya dengan menambah ruangan.", Arrays.asList(arrayEffect8)));
         Arrays.fill(arrayEffect8, null);
 
         /* Aksi Beli Barang */
         Random random = new Random();
         // random.nextInt(13) + 4 = [4..16]
-        Effect[] arrayEffect9 = {new Effect("", 0, random.nextInt(13) + 4)};
-        listAction.add(new Action("Buy Item",
-                "Sim bisa menghabiskan uang virtual untuk mengisi rumahnya dengan barang-barang. Karena situasi " +
-                        "pengiriman barang yang tidak pasti, maka waktu kedatangan barang tidak dapat dipastikan. " +
-                        "Maka diketahui bahwa durasi pengiriman barang akan selalu acak tetapi tetap dalam range " +
-                        "waktu X menit.",
-                Arrays.asList(arrayEffect9)));
+        Effect[] arrayEffect9 = {new Effect("Sim bisa menghabiskan uang virtual untuk mengisi rumahnya dengan barang-barang.", 0, random.nextInt(13) + 4)};
+        listAction.add(new Action("Buy Item", "", Arrays.asList(arrayEffect9)));
         Arrays.fill(arrayEffect9, null);
 
         /* Aksi Berpindah Ruangan */
-        listAction.add(new Action("Move Room",
-                "Sims dapat berpindah ruangan untuk melakukan interaksi pada objek yang terdapat pada ruangan berbeda" +
-                        "."));
+        listAction.add(new Action("Move Room", "Sims dapat berpindah ruangan untuk melakukan interaksi pada objek yang terdapat pada ruangan berbeda."));
 
         /* Aksi Melihat Inventory */
-        listAction.add(new Action("View Inventory",
-                "Sims dapat melihat inventory yang berisi dengan makanan, barang-barang yang sedang tidak terpasang " +
-                        "pada ruangan, dan objek-objek lainnya."));
+        listAction.add(new Action("View Inventory", "Sims dapat melihat inventory yang berisi dengan makanan, barang-barang yang sedang tidak terpasang pada ruangan, dan objek-objek lainnya."));
 
         /* Aksi Memasang Barang */
-        listAction.add(new Action("Install Item",
-                "Dalam dunia Sim-Plicity, pemasangan barang apapun dapat dilakukan dengan instan. Barang yang akan " +
-                        "dipasang harus muat dalam ruangan."));
+        listAction.add(new Action("Install Item", "Dalam dunia Sim-Plicity, pemasangan barang apapun dapat dilakukan dengan instan."));
 
         /* Aksi Melihat Waktu */
-        listAction.add(new Action("Check Time", "Tindakan ini membutuhkan objek Jam, dan akan menunjukkan sisa waktu " +
-                "pada hari tersebut beserta sisa waktu yang masih ada untuk seluruh tindakan yang bisa ditinggal."));
+        listAction.add(new Action("Check Time", "Tindakan ini akan menunjukkan sisa waktu pada hari tersebut beserta sisa waktu yang masih ada untuk seluruh tindakan yang bisa ditinggal."));
+
+        /* Aksi Buatan */
+        /* Aksi Lempar Kursi */
+        listAction.add(new Action("Throw Chair", "Kursi yang dilempar akan hilang."));
+
+        /* Aksi Naik Meja */
+        listAction.add(new Action("Climb Table", "Sims dapat naik ke atas meja."));
+
+        /* Aksi Menyalakan Kompor */
+        listAction.add(new Action("Turn On Stove", "Kompor harus dinyalakan terlebih dahulu untuk bisa memasak."));
+
+        /* Aksi Mematikan Kompor */
+        listAction.add(new Action("Turn Off Stove", "Kompor harus dinyalakan terlebih dahulu untuk bisa memasak."));
+
+        /* Aksi Duduk */
+        listAction.add(new Action("Sit", "Sims dapat duduk di kursi ataupun kasur."));
+
+        /* Aksi Buang Air Besar */
+        listAction.add(new Action("Defecate", "Sims dapat melakukan buang air besar di toilet."));
+
+        /* Aksi Cuci Tangan */
+        listAction.add(new Action("Wash Hand", "Sims dapat cuci tangan di wastafel."));
+
+        /* Aksi Bercermin */
+        listAction.add(new Action("Look Mirror", "Sims dapat bercermin di cermin."));
     }
 
     public String getActionName() {
