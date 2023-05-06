ffd783bd4f6ba08d74551f248cbe40edda874f04
Syafiq Ziyadul Arifin <SZArifin20041@gmail.com>
2023-05-06 14:30:49 +0700

    update gui

--- src/main/java/simplicity/GUIMain.java
+++ src/main/java/simplicity/GUIMain.java
@@ -9,6 +9,8 @@ import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -29,7 +31,8 @@ public class GUIMain extends Main {
         final GraphicsEnvironment GE = GraphicsEnvironment.getLocalGraphicsEnvironment();
         final List<String> AVAILABLE_FONT_FAMILY_NAMES = Arrays.asList(GE.getAvailableFontFamilyNames());
         try {
-            final File fontFile = new File(fontPath, "SimsLLHP.ttf");
+            // final File fontFile = new File(fontPath, "SimsLLHP.ttf");
+            final File fontFile = new File(fontPath, "Loucos Lyne.otf");
             if (fontFile.exists()) {
                 Font FONT = Font.createFont(Font.TRUETYPE_FONT, fontFile);
                 if (!AVAILABLE_FONT_FAMILY_NAMES.contains(FONT.getFontName())) {
@@ -41,28 +44,39 @@ public class GUIMain extends Main {
         }
     }
 
-    public void showMainFrame() {
-        JFrame frame = new JFrame("SimPlicity - Start Screen");
-        frame.setLocationRelativeTo(null);
-        // frame.setLayout(new FlowLayout());
+    public void setupFrame(JFrame frame) {
+        frame.setLayout(null);
         frame.setSize(1280, 720);
         frame.setResizable(false);
+    }
 
-        BufferedImage img;
-        try {
-            img = ImageIO.read(new File(imagePath, "startScreen.png"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public void resetupFrame(JFrame frame) {
+        frame.setSize(1280, 720);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public void addToFrame(JFrame frame, Component[] components) {
+        for (Component component : components) {
+            frame.add(component);
         }
+    }
 
-        ImageIcon icon = resizeImage(new ImageIcon(img), frame.getWidth(), frame.getHeight());
+    public void addToPanel(JPanel panel, Component[] components) {
+        for (Component component : components) {
+            panel.add(component);
+        }
+    }
 
-        JLabel label0 = new JLabel();
-        label0.setIcon(icon);
+    public void showMainFrame() {
+        JFrame frame = new JFrame("SimPlicity - Start Screen");
+        setupFrame(frame);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.add(createButton("Start Game", e -> {
+        JLabel label = createLabelImage("startScreen.png", 1280, 720);
+        label.setBounds(0, 0, 1280, 720);
+
+        JButton button1 = createButton("Start Game", e -> {
             String simName = JOptionPane.showInputDialog(frame, "Masukkan nama SIM:", "Create SIM", JOptionPane.QUESTION_MESSAGE);
             if (simName != null) {
                 while (equals(simName.trim(), "")) {
@@ -76,18 +90,18 @@ public class GUIMain extends Main {
                     showWorldFrame();
                 }
             }
-        }));
-        panel.add(createButton("About", e -> {
+        });
+        button1.setBounds((1280 - button1.getPreferredSize().width) / 2, 360, button1.getPreferredSize().width, button1.getPreferredSize().height);
+
+        JButton button2 = createButton("About", e -> {
             frame.setVisible(false);
             showAboutFrame();
-        }));
+        });
+        button2.setBounds((1280 - button2.getPreferredSize().width) / 2, 420, button2.getPreferredSize().width, button2.getPreferredSize().height);
 
-        frame.add(label0);
-        frame.add(panel);
+        addToFrame(frame, new Component[]{button1, button2, label});
 
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        resetupFrame(frame);
     }
 
     public JPanel createRoomInfo() {
@@ -135,6 +149,29 @@ public class GUIMain extends Main {
         return panel;
     }
 
+    public JLabel createLabelImage(String path, int width, int height) {
+        JLabel label = new JLabel();
+        label.setIcon(createImage(path, width, height));
+        return label;
+    }
+
+    public ImageIcon createImage(String path, int width, int height) {
+        BufferedImage img;
+        try {
+            img = ImageIO.read(new File(imagePath, path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ImageIcon icon = resizeImage(new ImageIcon(img), width, height);
+        return icon;
+    }
+
+    public JLabel createLabel(String text, int x, int y) {
+        JLabel label = createLabel(text);
+        label.setBounds((1280 - x - label.getPreferredSize().width) / 2, y, label.getPreferredSize().width, label.getPreferredSize().height);
+        return label;
+    }
+
     public JLabel createLabel(String text) {
         return createLabel(text, Component.LEFT_ALIGNMENT, 20);
     }
@@ -150,44 +187,57 @@ public class GUIMain extends Main {
     public JLabel createLabel(String text, float alignment, int fontSize) {
         JLabel label = new JLabel(text);
         label.setAlignmentX(alignment);
-        label.setFont(new Font("SimLLHP", Font.PLAIN, fontSize));
+        label.setFont(new Font("The Sims Sans", Font.PLAIN, fontSize));
+        label.setForeground(Color.white);
         return label;
     }
 
     public JButton createButton(String text, ActionListener callback) {
-        JButton button = new JButton(text);
+        return createButton(text, callback, "#a6e329");
+    }
+
+    public JButton createButton(String text, ActionListener callback, String colorCode) {
+        JButton button = new RoundedButton(text, 40, Color.decode(colorCode));
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
         button.addActionListener(callback);
-        button.setFont(new Font("SimLLHP", Font.PLAIN, 20));
         return button;
     }
 
     public void showAboutFrame() {
         JFrame frame = new JFrame("SimPlicity - About");
-        frame.setLayout(new FlowLayout());
-        frame.setResizable(false);
+        setupFrame(frame);
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-        panel.add(createLabel("IF2212 Pemrograman Berorientasi Objek STI", Component.CENTER_ALIGNMENT));
-        panel.add(createLabel(" "));
-        panel.add(createLabel("Kelompok 8", Component.CENTER_ALIGNMENT));
-        panel.add(createLabel("1. Michael Ryan Martin (18219046)", Component.CENTER_ALIGNMENT));
-        panel.add(createLabel("2. Syafiq Ziyadul Arifin (18221048)", Component.CENTER_ALIGNMENT));
-        panel.add(createLabel("3. Nixon Deflin Kurniawan (18221150)", Component.CENTER_ALIGNMENT));
-        panel.add(createLabel("4. Fredrick Runie Taslim (18221156)", Component.CENTER_ALIGNMENT));
-        panel.add(createLabel("5. Rania Sasi Kirana (18221168)", Component.CENTER_ALIGNMENT));
-
-        frame.add(panel);
-        frame.add(createButton("Back", e -> {
+        JLabel background = createLabelImage("background.png", 1280, 720);
+        background.setBounds(0, 0, 1280, 720);
+        JLabel label1 = createLabel("IF2212 Pemrograman Berorientasi Objek STI", 0, 150);
+        JLabel label2 = createLabel("Kelompok 8", 0, 200);
+        JLabel label3 = createLabel("1. Michael Ryan Martin (18219046)", 0, 250);
+        JLabel label4 = createLabel("2. Syafiq Ziyadul Arifin (18221048)", 0, 280);
+        JLabel label5 = createLabel("3. Nixon Deflin Kurniawan (18221150)", 0, 310);
+        JLabel label6 = createLabel("4. Fredrick Runie Taslim (18221156)", 0, 340);
+        JLabel label7 = createLabel("5. Rania Sasi Kirana (18221168)", 0, 370);
+        JLabel label8 = createLabel("https://github.com/Safiqq/OOPSTI-Tubes", 0, 420);
+        label8.setForeground(Color.BLUE.brighter());
+        label8.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        label8.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI("https://github.com/Safiqq/OOPSTI-Tubes"));
+                } catch (IOException | URISyntaxException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        JButton button = createButton("Back", e -> {
             frame.setVisible(false);
             showMainFrame();
-        }));
+        }, "#808080");
+        button.setBounds(40, 40, button.getPreferredSize().width, button.getPreferredSize().height);
 
-        frame.pack();
-        frame.setSize(720, 250);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        addToFrame(frame, new Component[]{label1, label2, label3, label4, label5, label6, label7, label8, button, background});
+
+        resetupFrame(frame);
     }
 
     public JPanel createRoom() {
@@ -255,15 +305,16 @@ public class GUIMain extends Main {
     public JPanel createWorld(JFrame frame) {
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(world.getWidth(), world.getLength()));
+        ImageIcon home = createImage("floor/floor2.png", 8, 8);
+        ImageIcon notHome = createImage("floor/grass.png", 8, 8);
 
         for (int i = 0; i < (world.getWidth() * world.getLength()); i++) {
-            JLabel label;
+            JLabel label = new JLabel();
             Point houseLoc = currentSim.getSimLoc().getHouse().getHouseLoc();
-            label = createLabel("       ", 6);
             if (i % world.getWidth() == houseLoc.getX() && i / world.getLength() == houseLoc.getY()) {
-                label.setBackground(new Color(166, 227, 41));
+                label.setIcon(home);
             } else {
-                label.setBackground(new Color(255, 255, 255));
+                label.setIcon(notHome);
             }
             label.setOpaque(true);
             label.setBorder(BorderFactory.createLineBorder(Color.BLACK));
@@ -296,35 +347,25 @@ public class GUIMain extends Main {
 
     public void showWorldFrame() {
         JFrame frame = new JFrame("World Map");
-        frame.setLayout(new FlowLayout());
-        frame.setResizable(false);
+        setupFrame(frame);
 
-        frame.add(createWorld(frame));
-        frame.add(createSimInfo());
-        frame.add(createButton("Back", e -> {
+        JLabel background = createLabelImage("background.png", 1280, 720);
+        background.setBounds(0, 0, 1280, 720);
+
+        JPanel panel = createWorld(frame);
+        panel.setBounds(80, 16, panel.getPreferredSize().width, panel.getPreferredSize().height);
+        //        frame.add(createSimInfo());
+        JButton button = createButton("Back", e -> {
             frame.setVisible(false);
             showMainFrame();
-        }));
+        });
 
-        frame.pack();
-        frame.setSize(1280, 720);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        addToFrame(frame, new Component[]{button, panel, background});
+
+        resetupFrame(frame);
     }
 
     public ImageIcon resizeImage(ImageIcon currentIcon, int width, int height) {
-        int nw = currentIcon.getIconWidth();
-        int nh = currentIcon.getIconHeight();
-
-        if (currentIcon.getIconWidth() > width) {
-            nw = width;
-            nh = (nw * currentIcon.getIconHeight()) / currentIcon.getIconWidth();
-        }
-        if (nh > height) {
-            nh = height;
-            nw = (currentIcon.getIconWidth() * nh) / currentIcon.getIconHeight();
-        }
-
-        return new ImageIcon(currentIcon.getImage().getScaledInstance(nw, nh, Image.SCALE_DEFAULT));
+        return new ImageIcon(currentIcon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));
     }
 }
---
+++ src/main/java/simplicity/RoundedButton.java
@@ -0,0 +1,60 @@
+package simplicity;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class RoundedButton extends JButton {
+
+    private final Color backgroundColor;
+    private int radius = 20;
+
+    public RoundedButton(String text, int radius, Color backgroundColor) {
+        super(text);
+        this.radius = radius;
+        this.backgroundColor = backgroundColor;
+        init();
+    }
+
+    private void init() {
+        setBackground(backgroundColor);
+        setOpaque(false);
+        setFont(new Font("The Sims Sans", Font.PLAIN, 24));
+        setForeground(Color.WHITE);
+        setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        setFocusPainted(false);
+        setContentAreaFilled(false);
+        setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setBackground(backgroundColor.darker());
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setBackground(backgroundColor);
+            }
+        });
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        super.paintComponent(g2);
+        g2.dispose();
+    }
+
+    @Override
+    protected void paintBorder(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getForeground());
+        g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, radius, radius);
+        g2.dispose();
+    }
+}
